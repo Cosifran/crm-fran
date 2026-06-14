5a3d3df1f8339c71113e5ceb73e99a1dba8f7dc5
Francisco Estrada <franciscobackend@gmail.com>
2026-06-14 14:26:48 -0500

    feat: initialize monorepo structure with Turborepo, tRPC, Drizzle, and authentication setup

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "../index.css";
+import Header from "@/components/header";
+import Providers from "@/components/providers";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "crm-fran",
+  description: "crm-fran",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning cz-shortcut-listen="true">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <div className="grid grid-rows-[auto_1fr] h-svh">
+            <Header />
+            {children}
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
