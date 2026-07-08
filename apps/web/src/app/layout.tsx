da90cc7dbf3e07be7f4631a5bd76ece1ae755897
Francisco Estrada <franciscobackend@gmail.com>
2026-07-08 18:54:43 -0500

    feat: scaffold comprehensive UI component library with base-ui and system-wide design tokens

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,6 +5,10 @@ import "../index.css";
 import Header from "@/components/header";
 import Providers from "@/components/providers";
 
+import { SiteHeader } from "@crm-fran/ui/components/site-header";
+import { AppSidebar } from "@crm-fran/ui/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@crm-fran/ui/components/sidebar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,10 +35,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <div className="grid grid-rows-[auto_1fr] h-svh">
-            <Header />
-            {children}
-          </div>
+          <SidebarProvider
+            style={
+              {
+                "--sidebar-width": "calc(var(--spacing) * 72)",
+                "--header-height": "calc(var(--spacing) * 12)",
+              } as React.CSSProperties
+            }
+          >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+              <SiteHeader />
+              <div className="flex flex-1 flex-col"> {children}</div>
+            </SidebarInset>
+          </SidebarProvider>
         </Providers>
       </body>
     </html>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,11 @@ import { auth } from "@crm-fran/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import Dashboard from "./dashboard/dashboard";
+import { DataTable } from "@crm-fran/ui/components/data-table";
+import { SectionCards } from "@crm-fran/ui/components/section-cards";
+import { ChartAreaInteractive } from "@crm-fran/ui/components/chart-area-interactive";
+
+import { dashboardData } from "@/constants/constanst";
 
 export default async function DashboardPage() {
   const session = await auth.api.getSession({
@@ -14,10 +18,14 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome {session.user.name}</p>
-      <Dashboard session={session} />
+    <div className="@container/main flex flex-1 flex-col gap-2">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <SectionCards />
+        <div className="px-4 lg:px-6">
+          <ChartAreaInteractive />
+        </div>
+        <DataTable data={dashboardData} />
+      </div>
     </div>
   );
 }
