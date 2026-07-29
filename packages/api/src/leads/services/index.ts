2b0496c9bf4bfdc6e9ca8d4039778184f6a72d71
Francisco Estrada <franciscobackend@gmail.com>
2026-07-28 19:32:26 -0500

    chore(api): export new services from index
    Implements: SH-LEADQA-002, SH-LEADQA-007
    - Exports partitionQASession, isCloserOf, hasCloserSession, recordCloserAnswers, adminEditLeadQASession.

--- packages/api/src/leads/services/index.ts
+++ packages/api/src/leads/services/index.ts
@@ -4,5 +4,22 @@ import { getByUserId } from "./get-by-user";
 import { assignLead } from "./assign-lead";
 import { assignLeadToCaller } from "./assign-to-caller";
 import { getWithoutAssigned } from "./get-without-assigned";
+import { partitionQASession } from "./partition-qa-session";
+import { isCloserOf } from "./is-closer-of";
+import { hasCloserSession } from "./has-closer-session";
+import { recordCloserAnswers } from "./record-closer-answers";
+import { adminEditLeadQASession } from "./admin-edit-lead-qa-session";
 
-export { getAll, getById, getByUserId, getWithoutAssigned, assignLead, assignLeadToCaller };
+export {
+	getAll,
+	getById,
+	getByUserId,
+	getWithoutAssigned,
+	assignLead,
+	assignLeadToCaller,
+	partitionQASession,
+	isCloserOf,
+	hasCloserSession,
+	recordCloserAnswers,
+	adminEditLeadQASession,
+};
